abaa9904191e36435034e19808d11e68c018794c
K SAKTHIVEL <120034628+sakthivel-programmer@users.noreply.github.com>
2024-03-20 08:45:47 +0530

    Add files via upload

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Styles from "./loading.module.css"
+function Loading(){
+  return (
+    <div className={Styles.loading}>
+
+    </div>
+  )
+};
+
+export default Loading;
