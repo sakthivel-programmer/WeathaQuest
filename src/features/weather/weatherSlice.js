d480b6807c38be11c8db42c9d91ff8824dab587b
K SAKTHIVEL <120034628+sakthivel-programmer@users.noreply.github.com>
2024-03-20 08:48:31 +0530

    Update weatherSlice.js

--- src/features/weather/weatherSlice.js
+++ src/features/weather/weatherSlice.js
@@ -1,7 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  weatherReport:""
+  weatherReport:"",
+  loading:false
 };
 
 
@@ -16,10 +17,14 @@ export const fetchWeatherReportAsync = createAsyncThunk(
     await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${payload.location}&apikey=0D0eMMvwb7QknIW1fFTjT1git2LtPaEI`)
     .then(res => res.json())
     .then( parseData => 
-      ThunkAPI.dispatch(setWeatherReport({report:parseData}))
+      ThunkAPI.dispatch(setWeatherReport({report:parseData}))    
     )
-    // // The value we return becomes the `fulfilled` action payload
-    // return response.data;
+    .then(
+      setTimeout(()=>(
+        // cut of loading animation
+        ThunkAPI.dispatch(setLoading())
+      ),2500)
+    )
   }
 );
 
@@ -35,12 +40,21 @@ export const weatherSlice = createSlice({
       // immutable state based off those changes
       // console.log(action.payload.report,"reducer")
       state.weatherReport = action.payload.report;
+      
+    },
+    setLoading: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      // console.log(action.payload.report,"reducer")
+      state.loading = !state.loading;
     }
   },
 
 });
 // exporting reducer action
-export const { setWeatherReport } = weatherSlice.actions;
+export const { setWeatherReport,setLoading } = weatherSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
